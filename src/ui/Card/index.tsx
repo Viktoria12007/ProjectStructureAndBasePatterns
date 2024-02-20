04ec40fa3899b64905b191c112b780e2682341aa
Виктория Голубова <v.golubova@denexy.com>
2024-02-20 12:40:34 +0400

    Created abstract component Card.tsx

---
+++ src/ui/Card/index.tsx
@@ -0,0 +1,37 @@
+import StarIcon from '../../assets/star.svg?react';
+import {FC} from "react";
+import './styles.css';
+import {Restaurant} from "../../api/api";
+
+type TProps = {
+    restaurant: Restaurant,
+    // onRaitingRestaurant: ({ id: string, raiting: number }) => void,
+}
+
+export const Card: FC<TProps> = ({ restaurant}) => {
+    return (
+        <li className="restaurants-list__item">
+            <div className="restaurants-list__wrap">
+                <img className="restaurants-list__img" src={restaurant.url}/>
+                <h3 className="restaurants-list__title">{restaurant.name}</h3>
+                <div className="restaurants-list__subtitle">{restaurant.description}</div>
+            </div>
+            <div className="restaurants-list__raiting">
+                {[...Array(5)].map((item, index) => {
+                    return <button
+                                key={index}
+                                className="restaurants-list__button"
+                                // onClick={() => onRaitingRestaurant({ id: restaurant.id, raiting: index + 1 })}
+                            >
+                                <StarIcon
+                                    className="restaurants-list__icon"
+                                    width={30}
+                                    height={30}
+                                    fill={index + 1 <= restaurant.raiting ? "red" : "transparent"}
+                                />
+                           </button>
+                })}
+            </div>
+        </li>
+    );
+}
